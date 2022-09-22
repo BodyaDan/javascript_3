bad41f1df7f99f5ae71395166f60ac1c28dda895
Bohdan <104310369+BodyaDan@users.noreply.github.com>
2022-09-22 19:58:36 +0300

    Update taskFour.js

--- taskFour.js
+++ taskFour.js
@@ -2,6 +2,6 @@ function filterRange(array, a, b) {
 	return array.splice(a, (b - a));
 }
 
-var mas = [1,2,3,4,5,6,7,8,9];
-var fMax = filterRange(mas, 2,6);
+let mas = [1,2,3,4,5,6,7,8,9];
+let fMax = filterRange(mas, 2,6);
 console.log(fMax);
