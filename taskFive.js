40cfc21455f25ffc4f29def6e84106b7348afe5d
Bohdan <104310369+BodyaDan@users.noreply.github.com>
2022-09-22 19:58:20 +0300

    Update taskFive.js

--- taskFive.js
+++ taskFive.js
@@ -1,10 +1,10 @@
 function camelize(str) {
-	var mas = str.split('-');
-	for (var i = 1; i < mas.length; i++) {
+	let mas = str.split('-');
+	for (let i = 1; i < mas.length; i++) {
 		mas[i] = mas[i].charAt(0).toUpperCase() + mas[i].slice(1);
 	}
 	return mas.join('');
 }
 
-var string = 'my-short-string';
+let string = 'my-short-string';
 console.log(camelize(string));
