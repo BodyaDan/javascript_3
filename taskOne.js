eef3dd566a919e25f3e33f97ae9af53e07dc5ce5
Bohdan <104310369+BodyaDan@users.noreply.github.com>
2022-09-22 19:58:51 +0300

    Update taskOne.js

--- taskOne.js
+++ taskOne.js
@@ -2,5 +2,5 @@ function getLast(arr) {
 	return arr[arr.length - 1];
 }
 
-var products = ['Bread', 'Banana', 'Milk', 'Crisps'];
+let products = ['Bread', 'Banana', 'Milk', 'Crisps'];
 console.log(getLast(products));
